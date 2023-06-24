18edfc67b0d6e8a8089a2218f505a802414bb7ef
Md. Jahangir Alam <jahangir.2012@outlook.com>
2023-06-24 12:55:29 +0600

    zzz

--- my-app/app.vue
+++ my-app/app.vue
@@ -1,3 +1,15 @@
 <template>
   <NuxtPage />
 </template>
+
+<script>
+import { defineComponent } from '@vue/composition-api'
+import { ref } from 'vue';
+import { Modal } from 'usemodal-vue3'
+
+export default defineComponent({
+  setup() {
+    
+  },
+})
+</script>
